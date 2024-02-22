0694ecd10a4f98f6e930c4194f5297698b724c3d
zzagir <zaynalov005@mail.ru>
2024-02-22 23:31:44 +0300

    fix: in activate-page password validation

--- src/app/activate-page/page.tsx
+++ src/app/activate-page/page.tsx
@@ -6,22 +6,22 @@ import React from "react"
 import Image from "next/image";
 import Button from "@/components/ui/Button/Button";
 import Input from "@/components/ui/Input/Input";
-import { useForm } from "react-hook-form";
+import {useForm} from "react-hook-form";
 import styles from './activate.module.scss'
-import { useAppSelector, useAppDispatch } from "@/store/features/hooks";
-import { useRouter } from 'next/navigation';
-import { fetchActivation } from "@/store/features/user/user.actions";
-import { IAuth } from "./activate.types";
+import {useAppSelector, useAppDispatch} from "@/store/features/hooks";
+import {useRouter} from 'next/navigation';
+import {fetchActivation} from "@/store/features/user/user.actions";
+import {IAuth} from "./activate.types";
 
 export default function Authentication() {
-    const { flag, isError, isLoaded } = useAppSelector(state => state.userAuthorization)
-    const { profileFromActivation } = useAppSelector(state => state.userRegistration)
+    const {flag, isError, isLoaded} = useAppSelector(state => state.userAuthorization)
+    const {profileFromActivation} = useAppSelector(state => state.userRegistration)
     const router = useRouter()
     const dispatch = useAppDispatch()
     const {
         register,
         handleSubmit,
-        formState: { errors },
+        formState: {errors},
     } = useForm({
         mode: "onBlur",
         defaultValues: {
@@ -46,20 +46,20 @@ export default function Authentication() {
                 <div className={styles.innerForm}>
                     <form onSubmit={handleSubmit(onSubmit)}>
                         <label>Email
-                            <div style={{ marginTop: '12px', marginBottom: '12px' }}>
+                            <div style={{marginTop: '12px', marginBottom: '12px'}}>
                                 <Input
-                                    style={{ color: 'black' }}
+                                    style={{color: 'black'}}
                                     //@ts-ignore
                                     register={register}
                                     name="email"
                                     type="text"
                                     placeholder="ivanov@gmail.com"
                                     options={{
-                                        required: "Обязательное поле",
+                                        required: 'Обязательное поле',
                                         pattern: {
                                             value: /^[A-Z0-9._%+-]+@[A-Z0-9-]+.+.[A-Z]{2,4}$/i,
-                                            message: 'Введите корректный ящик'
-                                        }
+                                            message: 'Введите корректный ящик',
+                                        },
                                     }}
                                     error={errors?.email?.message}
                                 />
@@ -68,42 +68,50 @@ export default function Authentication() {
                         </label>
                         <label>
                             <div className={styles.faggotPassword}><span>Пароль</span><span>Забыли пароль?</span></div>
-                            <div style={{ marginTop: '12px', marginBottom: '24px' }}>
+                            <div style={{marginTop: '12px', marginBottom: '24px'}}>
                                 <Input
-                                    style={{ color: 'black' }}
+                                    style={{color: 'black'}}
                                     //@ts-ignore
                                     register={register}
                                     name="password"
                                     type="password"
                                     placeholder="*********"
                                     options={{
-                                        required: "Обязательное поле",
-                                        minLength: { message: "Минимальная длинна 8 символов", value: 8 },
-                                        pattern: {
-                                            value: /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/,
-                                            message: 'Не менее 8 символов и одной буквы (лат)'
+                                        required: 'Обязательное поле',
+                                        minLength: {
+                                            message: 'Минимальная длинна 8 символов!',
+                                            value: 8,
+                                        },
+                                        validate: {
+                                            number: value => /\d/.test(value) || "Пароль должен содержать хотя бы одну цифру!",
+                                            noRussianChars: value => !/[А-Яа-яЁё]/.test(value) || "Пароль не должен содержать русских символов!",
+                                            letter: value => /[A-Za-z]/.test(value) || "Пароль должен содержать хотя бы одну букву!"
                                         }
+
                                     }}
                                     error={errors?.password?.message}
                                 />
                             </div>
-                            {isError && <p style={{ textAlign: "center", color: "red" }}>Неверные ящик или пароль</p>}
+                            {isError && <p style={{textAlign: "center", color: "red"}}>Неверные ящик или пароль</p>}
                         </label>
                         {/* Доделай кнопку с позиции дизейблед */}
-                        <Button color={"gray"} style={{ width: '100%', marginBottom: '24px' }}>
+                        <Button color={"gray"} style={{width: '100%', marginBottom: '24px'}}>
                             Войти
                         </Button>
-                        {isLoaded === true ? <p style={{ textAlign: "center", color: "aquamarine" }}>Ждем ответа сервера...</p> : null}
+                        {isLoaded === true ?
+                            <p style={{textAlign: "center", color: "aquamarine"}}>Ждем ответа сервера...</p> : null}
                     </form>
-                    <p className={styles.alreadyHaveAccount}>Впервые на нашем сайте? <span className={styles.login} onClick={() => router.push('/registration')}>Создайте аккаунт</span></p>
+                    <p className={styles.alreadyHaveAccount}>Впервые на нашем сайте? <span className={styles.login}
+                                                                                           onClick={() => router.push('/registration')}>Создайте аккаунт</span>
+                    </p>
                     <div className={styles.innerLine}>
-                        <hr className={styles.line} style={{ marginRight: '5px' }} />
+                        <hr className={styles.line} style={{marginRight: '5px'}}/>
                         <span>или</span>
-                        <hr className={styles.line} style={{ marginLeft: '5px' }} />
+                        <hr className={styles.line} style={{marginLeft: '5px'}}/>
                     </div>
                     <div className={styles.innerImg}>
                         <Image
-                            style={{ marginRight: "12px" }}
+                            style={{marginRight: "12px"}}
                             src="/img/vk.svg"
                             width={38}
                             height={38}
@@ -118,7 +126,7 @@ export default function Authentication() {
                     </div>
                 </div>
             </div>
-        </div >
+        </div>
     );
 };
 
