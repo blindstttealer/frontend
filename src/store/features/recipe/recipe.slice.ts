e6588512054d72755d770c1afcd7c56d50608438
max <mcnic@users.noreply.github.com>
2024-04-26 21:59:05 -0700

    feat: add add/remove to Favorite handlers to RecipeCard

---
+++ src/store/features/recipe/recipe.slice.ts
@@ -0,0 +1,55 @@
+import { createSlice } from '@reduxjs/toolkit'
+import {
+  fetchAddToFavorites,
+  fetchRemoveFromFavorites,
+} from '@/store/features/recipe/recipe.actions'
+import { IRecipe } from './recipe.types'
+
+interface IInitialState {
+  recipe?: IRecipe
+  error: null | any
+  isLoading: boolean
+  flag: boolean
+}
+
+const initialState: IInitialState = {
+  error: null,
+  isLoading: false,
+  flag: false,
+}
+
+const recipeFeed = createSlice({
+  name: 'recipeFeed',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+
+      // fetch add to favorites
+      .addCase(fetchAddToFavorites.pending, (state, action) => {
+        state.isLoading = true
+      })
+      .addCase(fetchAddToFavorites.fulfilled, (state, action) => {
+        state.isLoading = false
+        // state. = action.payload
+      })
+      .addCase(fetchAddToFavorites.rejected, (state, action) => {
+        state.isLoading = false
+        state.error = action.payload
+      })
+
+      // fetch remore reom favorites
+      .addCase(fetchRemoveFromFavorites.pending, (state, action) => {
+        state.isLoading = true
+      })
+      .addCase(fetchRemoveFromFavorites.fulfilled, (state, action) => {
+        state.isLoading = false
+      })
+      .addCase(fetchRemoveFromFavorites.rejected, (state, action) => {
+        state.isLoading = false
+        state.error = action.payload
+      })
+  },
+})
+
+export default recipeFeed.reducer
---
+++ src/store/features/recipe/recipe.types.ts
@@ -0,0 +1,43 @@
+export interface IRecipeContainer {
+    count: number;
+    next: string;
+    previous: null;
+    results: IRecipe[];
+}
+
+export interface IRecipe {
+    id: number;
+    title: string;
+    slug: string;
+    category: any[];
+    short_text: string;
+    preview_image: null;
+    author: Author;
+    pub_date: string;
+    tag: any[];
+    cooking_time: number;
+    total_comments_count: number;
+    total_views_count: number;
+    total_reactions_count: number;
+    reactions: any[];
+    activity_count: number;
+    is_favorite: boolean
+}
+
+export interface Author {
+    id: number;
+    username: string;
+    avatar: string;
+}
+
+export interface IRecipeInitialState {
+    recipes: {
+        feed: IRecipe,
+        feedActivity: IRecipe,
+        feedSubscriptions: IRecipe
+    },
+    isError: any,
+
+    isLoading: boolean,
+    flag: boolean,
+}
