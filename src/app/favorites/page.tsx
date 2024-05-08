d798213a25c8c793a26b5315bd2a0cf27b9efae9
max <mcnic@users.noreply.github.com>
2024-05-08 15:08:32 -0700

    fefactor: make View for RecipiesList to Props

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -4,12 +4,15 @@ import styles from './favorites.module.scss'
 import { useFavorites } from '@/hooks/useFavorites'
 import ListViewChanger from '@/components/ui/listViewChanger/ListViewChanger'
 import RecipeList from '@/components/ui/RecipeList/RecipeList'
+import { useAppSelector } from '@/store/features/hooks'
 
 export default function Favorites() {
+  const { view } = useAppSelector((state) => state.recipesFeed)
+
   return (
     <Layout isSearch={true} rightbar={false}>
       <div className={`${styles.recipesContainer} scroll scroll--left scroll__thin`}>
-        <RecipeList dispatcher={useFavorites} />
+        <RecipeList dispatcher={useFavorites} view={view}/>
       </div>
       <div className={styles.rightbar}>
         <ListViewChanger />
