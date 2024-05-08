9f771d2cf121e401ba234102b17b411e0b34148e
max <mcnic@users.noreply.github.com>
2024-05-08 10:06:41 -0700

    feat: add temporary datepicker on Profile Page

--- src/components/ui/MyRecipies/MyRecipies.tsx
+++ src/components/ui/MyRecipies/MyRecipies.tsx
@@ -1,29 +1,22 @@
 'use client'
 
-import Image from 'next/image'
-import { FC, useEffect, useMemo } from 'react'
+import { FC, FormEvent, useMemo, useState } from 'react'
 import styles from './MyRecipies.module.scss'
-import {
-  useGetCurentUserDataQuery,
-  useLazyGetUserDataQuery,
-} from '@/store/features/user/user.actions'
-// import { useAuth } from '@/hooks/useAuth'
-import Layout from '@/components/layout/layout'
-// import { Loader } from '@/components/ui/Loader/Loader'
+import { useLazyGetUserDataQuery } from '@/store/features/user/user.actions'
 import RecipeList from '@/components/ui/RecipeList/RecipeList'
-import Tabs, { TabData } from '@/components/ui/Tabs/Tabs.module'
+import { TabData } from '@/components/ui/Tabs/Tabs.module'
 import Subscriptions from '@/components/ui/Subscriptions/Subscriptions'
 import Subscribers from '@/components/ui/Subscribers/Subscribers'
 import { getUseMyRecipies } from '@/hooks/useMyRecipies'
-import { Loader } from '@/components/ui/Loader/Loader'
-import ListLoader from '../ListLoader/ListLoader'
 import Button from '../Button/Button'
+import { Loader } from '../Loader/Loader'
 
 type MyRecipiesProps = {
   username?: string
 }
 
 const MyRecipies: FC<MyRecipiesProps> = ({ username }) => {
+  const [date, setDate] = useState<string>('')
   const [trigger, { data, error, isError, isLoading, isFetching }] =
     useLazyGetUserDataQuery()
 
@@ -48,26 +41,40 @@ const MyRecipies: FC<MyRecipiesProps> = ({ username }) => {
     [username],
   )
 
-  const selectSortByDateHandler = () => {
-    console.log('selectSortByDateHandler')
+  const handleSelectSortByDate = () => {
+    console.log('handleSelectSortByDate')
   }
 
-  const selectSortByComponentsHandler = () => {
-    console.log('selectSortByComponentsHandler')
+  const handleSelectSortByComponents = () => {
+    console.log('handleSelectSortByComponents')
+  }
+
+  //todo: После утверждения дизайна переделать на DatePicker
+  const handleDateChange = (event: FormEvent<HTMLInputElement>) => {
+    console.log('handleDateChange', event.currentTarget.value)
+    setDate(event.currentTarget.value)
   }
 
   if (error) return <div>{String(error)}</div>
+
+  if (isLoading) return <Loader />
 
   return (
     <div className={styles.container}>
       <div className={styles.top}>
         <div className={styles.buttons}>
-          <Button onClick={selectSortByDateHandler}>По дате</Button>
-          <Button onClick={selectSortByComponentsHandler}>
+          <Button onClick={handleSelectSortByDate}>По дате</Button>
+          <Button onClick={handleSelectSortByComponents}>
             По ингридиентам
           </Button>
         </div>
-        calnendar
+
+        <input
+          type="date"
+          value={date}
+          onChange={handleDateChange}
+          title="calendar"
+        />
       </div>
 
       {username && <RecipeList dispatcher={getUseMyRecipies(username)} />}
