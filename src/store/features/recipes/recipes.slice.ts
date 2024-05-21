66e66d419717e0482c8ac8e963b28b16e1496ef7
max <mcnic@users.noreply.github.com>
2024-05-21 10:26:45 -0700

    fix: Profile Page layout and styles

--- src/store/features/recipes/recipes.slice.ts
+++ src/store/features/recipes/recipes.slice.ts
@@ -2,16 +2,22 @@ import { createSlice } from '@reduxjs/toolkit'
 import { RecipeListVariant } from '@/hooks/useRecipes'
 
 export type RecipeView = 'feed' | 'tile'
+export type MyRecipeSort = 'date' | 'ingredients'
+
 interface IInitialState {
   view: RecipeView
   sort: RecipeListVariant
+  sortMyRecipes: MyRecipeSort
+  sortMyRecipesDate?: string // date with format 'yyyy-mm-dd'
 }
 
 const initialState: IInitialState = {
   view: 'feed',
   sort: 'top',
+  sortMyRecipes: 'date',
 }
 
+// todo: сделать перманентное хранение в local storage
 const recipesSettings = createSlice({
   name: 'recipesFeed',
   initialState,
@@ -22,8 +28,14 @@ const recipesSettings = createSlice({
     setSortMode: (state, action) => {
       state.sort = action.payload
     },
+    setSortMyRecipesMode: (state, action) => {
+      state.sortMyRecipes = action.payload
+    },
+    setDateSortMyRecipes: (state, action) => {
+      state.sortMyRecipesDate = action.payload
+    },
   },
 })
 
-export const { setViewMode, setSortMode } = recipesSettings.actions
+export const { setViewMode, setSortMode, setSortMyRecipesMode, setDateSortMyRecipes } = recipesSettings.actions
 export default recipesSettings.reducer
