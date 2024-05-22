5826d12ce0284580e99187c2871ac1b8896b52b0
max <mcnic@users.noreply.github.com>
2024-05-21 17:38:03 -0700

    fix: main menu

--- src/components/layout/sidebar/sidebar.tsx
+++ src/components/layout/sidebar/sidebar.tsx
@@ -4,162 +4,105 @@ import styles from './sidebar.module.scss'
 import Image from 'next/image'
 import { usePathname, useRouter } from 'next/navigation'
 import cn from 'clsx'
+import NavLink from '@/components/ui/NavLink/NavLink'
+
+type MenuItem = {
+  text: string
+  img: string
+  alt: string
+  path: string
+}
+
+const menu: MenuItem[] = [
+  {
+    text: 'Домой',
+    img: '/img/sidebar/home.svg',
+    alt: 'home',
+    path: '/',
+  },
+  {
+    text: 'Уведомления',
+    img: '/img/sidebar/notifications.svg',
+    alt: 'notifications',
+    path: '/notifications',
+  },
+  {
+    text: 'Закладки',
+    img: '/img/sidebar/favorites.svg',
+    alt: 'favorites',
+    path: '/favorites',
+  },
+  {
+    text: 'Профиль',
+    img: '/img/sidebar/user.svg',
+    alt: 'profile',
+    path: '/profile',
+  },
+  {
+    text: 'Настройки',
+    img: '/img/sidebar/settings.svg',
+    alt: 'setting',
+    path: '/setting',
+  },
+]
 
 export default function Sidebar() {
   const { isAuth } = useAuth()
   const router = useRouter()
   const pathname = usePathname()
 
+  const jsxIsAuth = (
+    <div className={styles.auth}>
+      {menu.map(({ text, img, alt, path }, key) => (
+        <NavLink
+          key={key}
+          text={text}
+          img={img}
+          alt={alt}
+          url={path}
+          active={pathname === path}
+        />
+      ))}
+    </div>
+  )
+
+  const jsxIsNotAuth = (
+    <div className={styles.notAuth}>
+      <button
+        className={styles.button}
+        onClick={() => router.push('activate-page')}
+      >
+        Войти
+      </button>
+      <button
+        className={styles.button}
+        onClick={() => router.push('registration')}
+      >
+        Регистрация
+      </button>
+    </div>
+  )
+
   return (
     <div className={styles.sidebar}>
-      {isAuth ? (
-        <div className={styles.auth}>
-          {/* 
-          WIP. для WCAG лучше делать линками, 
-          todo: сделать отдельный коспонент для пункта меню 
-          */}
-          {/* <Link className={styles.link} href="/">
-            <p>Домой</p>
-            <Image
-              src={
-                pathname === '/'
-                  ? '/img/sidebar/active/home.png'
-                  : '/img/sidebar/home.png'
-              }
-              alt="home"
-              width={22}
-              height={22}
-            />
-          </Link> */}
-          <button
-            className={cn(styles.buttons, {
-              [styles.active]: pathname === '/',
-            })}
-            onClick={() => router.push('/')}
-          >
-            <p>Домой</p>
-            <Image
-              src={
-                pathname === '/'
-                  ? '/img/sidebar/active/home.png'
-                  : '/img/sidebar/home.png'
-              }
-              alt="home"
-              width={22}
-              height={22}
-            />
-          </button>
-          <button
-            className={cn(styles.buttons, {
-              [styles.active]: pathname === '/notification',
-            })}
-            onClick={() => router.push('/notifications')}
-          >
-            <p>Уведомления</p>
-            <Image
-              src={
-                pathname === '/notifications'
-                  ? '/img/sidebar/active/notification.png'
-                  : '/img/sidebar/notification.png'
-              }
-              alt="home"
-              width={22}
-              height={22}
-            />
-          </button>
-          <button
-            className={cn(styles.buttons, {
-              [styles.active]: pathname === '/favorites',
-            })}
-            onClick={() => router.push('/favorites')}
-          >
-            <p>Закладки</p>
-            <Image
-              src={
-                pathname === '/favorites'
-                  ? '/img/sidebar/active/save.png'
-                  : '/img/sidebar/save.png'
-              }
-              alt="home"
-              width={22}
-              height={22}
-            />
-          </button>
-          <button
-            className={cn(styles.buttons, {
-              [styles.active]: pathname === '/profile',
-            })}
-            onClick={() => router.push('/profile')}
-          >
-            <p>Профиль</p>
-            <Image
-              src={
-                pathname === '/profile'
-                  ? '/img/sidebar/active/profile.png'
-                  : '/img/sidebar/profile.png'
-              }
-              alt="home"
-              width={22}
-              height={22}
-            />
-          </button>
-          <button
-            className={cn(styles.buttons, {
-              [styles.active]: pathname === '/setting',
-            })}
-            onClick={() => router.push('/setting')}
-          >
-            <p>Настройки</p>
-            <Image
-              src={
-                pathname === '/setting'
-                  ? '/img/sidebar/active/setting.png'
-                  : '/img/sidebar/setting.png'
-              }
-              alt="home"
-              width={22}
-              height={22}
-            />
-          </button>
-          <hr className={styles.hr} />
-          <div className={styles.mobileApp}>
-            <Image
-              src="/img/sidebar/mobileapp.png"
-              alt="mobile app"
-              width={28}
-              height={28}
-              draggable={false}
-            />
-            <p>Скачайте мобильное приложение </p>
-          </div>
-        </div>
-      ) : (
-        <div className={styles.notAuth}>
-          <button
-            className={styles.button}
-            onClick={() => router.push('activate-page')}
-          >
-            Войти
-          </button>
-          <button
-            className={styles.button}
-            onClick={() => router.push('registration')}
-          >
-            Регистрация
-          </button>
-          <hr className={styles.hr} />
-          <div className={styles.mobileApp}>
-            <Image
-              src="/img/sidebar/mobileapp.png"
-              alt="mobile app"
-              width={28}
-              height={28}
-              draggable={false}
-            />
-            <p>Скачайте мобильное приложение </p>
-          </div>
-        </div>
-      )}
+      {isAuth ? jsxIsAuth : jsxIsNotAuth}
+
+      <div className={styles.more}>
+        <p>FAQ</p>
+        <p>Правила сайта</p>
+        <p>Еще</p>
+      </div>
+
+      <div className={styles.mobileApp}>
+        <Image
+          src="/img/sidebar/mobileapp.png"
+          alt="mobile app"
+          width={28}
+          height={28}
+          draggable={false}
+        />
+        <p>Скачайте мобильное приложение </p>
+      </div>
     </div>
   )
 }
