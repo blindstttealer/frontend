d0e2aa0cb0fd13f62763d00bd7b4a165303e39f7
max <mcnic@users.noreply.github.com>
2024-06-03 17:58:39 -0700

    fix: bug 45. Fix global layout

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,31 +1,25 @@
-"use client"
-import { ReactNode } from "react";
-import Header from "@/components/layout/header/header";
-import Sidebar from "@/components/layout/sidebar/sidebar";
-import Rightbar from "@/components/layout/rightbar/rightbar";
+'use client'
+import { FC, ReactNode } from 'react'
+import Header from '@/components/layout/header/header'
+import Sidebar from '@/components/layout/sidebar/sidebar'
 import styles from './layout.module.scss'
-import { usePathname, useRouter } from "next/navigation";
 
-export default function Layout({ children, sidebar = true, rightbar = true, isSearch }: {
-    children: ReactNode,
-    sidebar?: boolean,
-    rightbar?: boolean,
-    isSearch?: boolean | undefined,
-}) {
-    const pathname = usePathname()
-    return (
-        <div className={styles.layout}>
-            <div className='container' style={{ flexDirection: "column" }}>
-                <Header isSearch={isSearch} />
-                <div className={styles.layoutTwo}>
-                    {sidebar && <Sidebar />}
-                    {children}
-                    {/* было */}
-                    {/* {rightbar && pathname === '/' && <Rightbar />} */}
-                    {/* изменил на */}
-                    {rightbar && <Rightbar />}
-                </div>
-            </div>
-        </div>
-    )
+type Props = {
+  children: ReactNode
+  sidebar?: boolean
+  isSearch?: boolean | undefined
 }
+
+const Layout: FC<Props> = ({ children, sidebar = true, isSearch }) => (
+  <div className={styles.layout}>
+    <div className="container" style={{ flexDirection: 'column' }}>
+      <Header isSearch={isSearch} />
+      <div className={styles.layoutTwo}>
+        {sidebar && <Sidebar />}
+        {children}
+      </div>
+    </div>
+  </div>
+)
+
+export default Layout
