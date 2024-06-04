9e1919992217a22aff479a918d86ff57bdcae4f7
bartcevich <bartcevich@tut.by>
2024-06-04 20:55:50 +0300

    The full recipe view component

--- src/components/ui/RecipeList/RecipeList.tsx
+++ src/components/ui/RecipeList/RecipeList.tsx
@@ -3,11 +3,13 @@
 import { FC, useEffect, useRef, useState } from 'react'
 import styles from './RecipeList.module.scss'
 import RecipeCard from '@/components/ui/RecipeCard/RecipeCard'
+import RecipeModify from '@/components/ui/RecipeModify/RecipeModify'
 import { RecipeListDispatcher } from '@/hooks/useFavorites'
 import EmptyRecipeList from './EmptyRecipeList'
 import ListLoader from '../ListLoader/ListLoader'
 import { ListLoadingError } from '../ListLoadingError/ListLoadingError'
 import { RecipeView } from '@/store/features/recipes/recipes.slice'
+import { id } from 'date-fns/locale/id'
 
 const RecipeList: FC<{
   dispatcher: RecipeListDispatcher
@@ -17,6 +19,7 @@ const RecipeList: FC<{
   const [containerStyles, setContainerStyles] = useState<string[]>([
     styles.wrapper,
   ])
+  const [activeIndex, setActiveIndex] = useState<number | null>(null)
 
   const { recipies, loadNextPageRef, isLoading, error } = dispatcher()
 
@@ -53,16 +56,43 @@ const RecipeList: FC<{
 
   if (error) return <ListLoadingError error={error} />
 
+  const toggleIngredients = (index: number, id: number) => {
+    console.log(index, id)
+    setActiveIndex(id)
+    // const updatedMenuData = menuData.map((item, i) => {
+    //   if (i === index) {
+    //     return { ...item, showIngredients: !item.showIngredients };
+    //   }
+    //   return item;
+    // });
+  }
+
+  const handleToggle = (index: number) => {}
+
   return (
     <div className={styles.container}>
       <div className={containerStyles.join(' ')}>
         {recipies?.length ? (
-          recipies?.map((recipe) => (
-            <RecipeCard
-              key={recipe.id}
-              recipe={recipe}
-              refreshListOnRemoveFromFavorites={true}
-            />
+          recipies?.map((recipe, index) => (
+            <div key={recipe.id}>
+              {activeIndex === recipe.id && (
+                <div className={styles.container_popup}>
+                  <RecipeModify
+                    key={recipe.id}
+                    recipe={recipe}
+                    refreshListOnRemoveFromFavorites={true}
+                  />
+                </div>
+              )}
+              <button onClick={() => toggleIngredients(index, recipe.id)}>
+                +
+              </button>
+              <RecipeCard
+                key={recipe.id}
+                recipe={recipe}
+                refreshListOnRemoveFromFavorites={true}
+              />
+            </div>
           ))
         ) : (
           <EmptyRecipeList />
