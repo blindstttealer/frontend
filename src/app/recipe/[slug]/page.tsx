5a0e252e47a7afe5dd475bee686578ffc326ad12
bartcevich <bartcevich@tut.by>
2024-09-06 20:33:02 +0300

    viewing a recipe with ingredients in a separate component

--- src/app/recipe/[slug]/page.tsx
+++ src/app/recipe/[slug]/page.tsx
@@ -1,11 +1,12 @@
 'use client'
 
-import { useEffect } from 'react'
+import { useState, useEffect } from 'react'
 
 import styles from './recipe.module.scss'
 import { useGetRecipeQuery } from '@/store/features/recipes/recipes.actions'
 import ButtonBack from '@/components/ui/ButtonBack/ButtonBack'
 import Link from 'next/link'
+import RecipeModify from '@/components/ui/RecipeModify/RecipeModify'
 
 export default function Recipe({ params }: { params: { slug: string } }) {
   const { data, isFetching, isLoading, error, status } = useGetRecipeQuery(
@@ -15,7 +16,6 @@ export default function Recipe({ params }: { params: { slug: string } }) {
   useEffect(() => {
     console.log({ data, isFetching, isLoading, error, status })
   }, [data, error, isFetching, isLoading, status])
-
   if (error)
     return (
       <div className={styles.wrongSlug}>
@@ -27,13 +27,11 @@ export default function Recipe({ params }: { params: { slug: string } }) {
   //todo поместить сюда форму просмотра рецепта
   return (
     <>
-      Просмотр рецепта
-
       <Link href={`/recipe/edit/${params.slug}`}>Изменить</Link>
-
-      <pre style={{ display: 'block', fontSize: '10pt' }}>
+      {/* <pre style={{ display: 'block', fontSize: '10pt' }}>
         {JSON.stringify(data, null, 2)}
-      </pre>
+      </pre> */}
+      {data && <RecipeModify recipe={data} slug={''} pub_date={''} />}
     </>
   )
 }
